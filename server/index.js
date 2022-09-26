34f719b98afec1abe4247e53a4c61830d6d3625b
JuanIsa <JuanIsa>
2022-09-26 09:49:00 -0300

    1.1.4

--- server/index.js
+++ server/index.js
@@ -1,32 +1,47 @@
 import express from 'express';
 import morgan from 'morgan';
-import {Server as socketServer } from 'socket.io';
+import { Server as socketServer } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
 const server = http.createServer(app);
 const io = new socketServer(server, {
     cors: {
-        origin:'*'
+        origin: '*'
     }
 })
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(morgan('dev'));
 
 io.on('connection', (socket) => {
-    socket.on('mensajeDesdeFront', value => { 
-        socket.broadcast.emit('mensajedelBak', {
+    let canal='';
+    socket.on('subscribe', room => {
+        console.log('Se unió a la sala '+room);
+        socket.join(room);
+        canal = room;
+    });
+
+    socket.on('send message', data => {
+        console.log(data.room);
+        socket.broadcast.to(canal).emit('back', {
             usuario: socket.id,
-            contenido: value
+            contenido: data
         });
     });
+    // socket.on('mensajeDesdeFront', value => { 
+
+    //     socket.broadcast.emit('mensajedelBak', {
+    //         usuario: socket.id,
+    //         contenido: value
+    //     });
+    // });
 })
 
 
 
 server.listen(PORT);
-console.log('Server en puerto '+PORT);
+console.log('Server en puerto ' + PORT);
